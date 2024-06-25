60573b422e7f5386ca3e367a8b73ebe5d876bd04
naviya2003 <amigolk6@gmail.com>
2024-06-25 22:10:49 +0530

    portfolio website

---
+++ My portfolio/script.js
@@ -0,0 +1,36 @@
+// Select the button element
+const downloadBtn = document.getElementById('downloadBtn');
+
+// Add a click event listener to the button
+downloadBtn.addEventListener('click', () => {
+    // Replace 'path_to_your_cv.pdf' with the actual path to your CV file
+    const cvPath = 'https://getyourcv.net/wp-content/uploads/2023/05/CV-EN-1.docx';
+
+    // Create an anchor element
+    const link = document.createElement('a');
+    link.href = cvPath;
+
+    // Set the download attribute with your desired file name
+    link.download = 'Resume';
+
+    // Append the anchor element to the body
+    document.body.appendChild(link);
+
+    // Trigger the click event of the anchor element
+    link.click();
+
+    // Clean up
+    document.body.removeChild(link);
+});
+
+
+
+// You can add JavaScript for interactivity here if needed
+// Example: toggle a class on click
+// const projectItems = document.querySelectorAll('.project');
+// projectItems.forEach(item => {
+//     item.addEventListener('click', () => {
+//         item.classList.toggle('active');
+//     });
+// });
+
